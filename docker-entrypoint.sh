770462363755556fd813bf51ea8ac1e55eed3e15
izumiya <92346+izumiya@users.noreply.github.com>
2019-08-16 01:15:10 +0900

    Fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$PORT" ] ; then
-    sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/sites-enabled
+    sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/sites-enabled/default
 fi
 
 exec "$@"
