8d5268099e22287a06bcae7ead9b570f7747aab8
izumiya <92346+izumiya@users.noreply.github.com>
2019-08-16 01:27:18 +0900

    Bugfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ -z "$PORT" ] ; then
+if [ ! -z "$PORT" ] ; then
     sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/sites-enabled/default
+    sed -i "s/127.0.0.1:80/127.0.0.1:${PORT}/" /etc/supervisor/conf.d/oauth2_proxy.conf
 fi
 
 exec "$@"
